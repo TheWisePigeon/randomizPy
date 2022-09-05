4747e1f12cdee549e196ea570695287bc6a5af51
TheWisePigeon <zozozozeph@gmail.com>
2022-09-05 23:45:49 +0000

    feat: added basic function to get parameters

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from flask import Flask, jsonify
+import json
+from flask import Flask, jsonify, request
 import os
 
 app = Flask(__name__)
@@ -8,6 +9,23 @@ def rootRoute():
     return jsonify({
         "message": 'Sup bitch'
 })
+
+@app.route('/generate', methods=['POST'])
+def generateDate():
+    data = request.get_json()
+    try:
+        rows = data["rows"]
+        format = data["format"]
+        schema = data["schema"]
+    except:
+        return jsonify({
+            "message": "Key required"
+        })
+    schema = eval(schema)
+    return jsonify({
+        "you sent": data
+    })
+    
 
 if __name__ == '__main__':
     app.run(debug=True,port=os.getenv("port", default=5500))
